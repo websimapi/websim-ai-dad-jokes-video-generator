5cb211b51a83befcced8993d0f85dc201d06bd1c
api <api@websim.ai>
2025-11-06 13:32:35 +0000

    v2: I only see white screen

--- composition.jsx
+++ composition.jsx
@@ -1,88 +1,96 @@
 import { jsxDEV } from "react/jsx-dev-runtime";
 import React from "react";
-import { AbsoluteFill, Series, Audio, staticFile, continueRender, delayRender } from "remotion";
+import { AbsoluteFill, Series, Audio, staticFile } from "remotion";
 import { Joke } from "./Joke.jsx";
 const SETUP_DURATION = 90;
 const PUNCHLINE_DURATION = 120;
 const TRANSITION_DURATION = 15;
 const DadJokeComposition = ({ jokes }) => {
-  if (!jokes) {
-    return null;
+  if (!jokes || jokes.length === 0) {
+    return /* @__PURE__ */ jsxDEV(AbsoluteFill, { style: { backgroundColor: "#4158D0", justifyContent: "center", alignItems: "center" }, children: /* @__PURE__ */ jsxDEV(Joke, { text: "Click the button to generate the first dad joke!" }, void 0, false, {
+      fileName: "<stdin>",
+      lineNumber: 15,
+      columnNumber: 17
+    }) }, void 0, false, {
+      fileName: "<stdin>",
+      lineNumber: 14,
+      columnNumber: 13
+    });
   }
   return /* @__PURE__ */ jsxDEV(AbsoluteFill, { style: { backgroundColor: "#4158D0" }, children: [
     /* @__PURE__ */ jsxDEV(Series, { children: jokes.map((joke) => /* @__PURE__ */ jsxDEV(React.Fragment, { children: [
       /* @__PURE__ */ jsxDEV(Series.Sequence, { durationInFrames: SETUP_DURATION, children: /* @__PURE__ */ jsxDEV(AbsoluteFill, { style: { justifyContent: "center", alignItems: "center" }, children: [
         /* @__PURE__ */ jsxDEV(Joke, { text: joke.setup }, void 0, false, {
           fileName: "<stdin>",
-          lineNumber: 22,
+          lineNumber: 27,
           columnNumber: 33
         }),
         joke.setupAudioUrl && /* @__PURE__ */ jsxDEV(Audio, { src: joke.setupAudioUrl }, void 0, false, {
           fileName: "<stdin>",
-          lineNumber: 23,
+          lineNumber: 28,
           columnNumber: 56
         })
       ] }, void 0, true, {
         fileName: "<stdin>",
-        lineNumber: 21,
+        lineNumber: 26,
         columnNumber: 29
       }) }, void 0, false, {
         fileName: "<stdin>",
-        lineNumber: 20,
+        lineNumber: 25,
         columnNumber: 25
       }),
       /* @__PURE__ */ jsxDEV(Series.Sequence, { durationInFrames: PUNCHLINE_DURATION, name: "Punchline", children: /* @__PURE__ */ jsxDEV(AbsoluteFill, { style: { justifyContent: "center", alignItems: "center" }, children: [
         /* @__PURE__ */ jsxDEV(Joke, { text: joke.punchline }, void 0, false, {
           fileName: "<stdin>",
-          lineNumber: 28,
+          lineNumber: 33,
           columnNumber: 33
         }),
         joke.punchlineAudioUrl && /* @__PURE__ */ jsxDEV(Audio, { src: joke.punchlineAudioUrl }, void 0, false, {
           fileName: "<stdin>",
-          lineNumber: 29,
+          lineNumber: 34,
           columnNumber: 60
         }),
         /* @__PURE__ */ jsxDEV(Audio, { src: staticFile("laugh_track.mp3"), startFrom: 30, volume: 0.7 }, void 0, false, {
           fileName: "<stdin>",
-          lineNumber: 30,
+          lineNumber: 35,
           columnNumber: 33
         }),
         /* @__PURE__ */ jsxDEV(Audio, { src: staticFile("groan.mp3"), startFrom: 45, volume: 0.7 }, void 0, false, {
           fileName: "<stdin>",
-          lineNumber: 31,
+          lineNumber: 36,
           columnNumber: 33
         })
       ] }, void 0, true, {
         fileName: "<stdin>",
-        lineNumber: 27,
+        lineNumber: 32,
         columnNumber: 29
       }) }, void 0, false, {
         fileName: "<stdin>",
-        lineNumber: 26,
+        lineNumber: 31,
         columnNumber: 25
       }),
       /* @__PURE__ */ jsxDEV(Series.Sequence, { durationInFrames: TRANSITION_DURATION }, void 0, false, {
         fileName: "<stdin>",
-        lineNumber: 34,
+        lineNumber: 39,
         columnNumber: 25
       })
     ] }, joke.id, true, {
       fileName: "<stdin>",
-      lineNumber: 19,
+      lineNumber: 24,
       columnNumber: 21
     })) }, void 0, false, {
       fileName: "<stdin>",
-      lineNumber: 17,
+      lineNumber: 22,
       columnNumber: 13
     }),
     /* @__PURE__ */ jsxDEV(Audio, { src: staticFile("background_music.mp3"), loop: true, volume: 0.15 }, void 0, false, {
       fileName: "<stdin>",
-      lineNumber: 38,
+      lineNumber: 43,
       columnNumber: 13
     })
   ] }, void 0, true, {
     fileName: "<stdin>",
-    lineNumber: 16,
+    lineNumber: 21,
     columnNumber: 9
   });
 };
